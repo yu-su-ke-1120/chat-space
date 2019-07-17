491333b17bcab5bb1e8ff4c67f07509ea20aaace
yu-su-ke-1120 <yusuke-9124@docomo.ne.jp>
2019-07-17 20:35:24 +0900

    update chatspace

--- config/application.rb
+++ config/application.rb
@@ -14,9 +14,9 @@ module ChatSpace
       g.helper false
       g.test_framework false
     end
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja
   end
 end

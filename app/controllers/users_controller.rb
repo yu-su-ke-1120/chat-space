47d99494d6abf597858c0d3a24890a23e11a713d
yu-su-ke-1120 <yusuke-9124@docomo.ne.jp>
2019-08-16 18:55:47 +0900

    Update users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    @users = User.where('name LIKE(?) AND id != ?', "%#{params[:keyword]}%", current_user)
     respond_to do |format|
       format.html
       format.json
